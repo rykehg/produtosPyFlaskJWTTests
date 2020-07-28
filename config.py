b995523253c88dc85f39c60d6893bd65eb4ab3bb
Henrique Gaeta <46508056+rykehg@users.noreply.github.com>
2020-07-28 12:15:50 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,7 @@ MySQL = {
     'user': 'user',
     'password': 'password',
     'host': 'host:port',
-<<<<<<< HEAD
-    'database': 'databaseName',
-=======
     'database': 'databaseName'
->>>>>>> a6a7f7294bb0b75ec57e48bf7fde1d47b31c01df
 }
 
 SQLAlchemyMod = False
